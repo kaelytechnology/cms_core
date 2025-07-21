8331739cb88187d1fea7de17fdee756934e9769c
Rafael Mendoza <kaelytechnology@gmail.com>
2025-07-20 18:55:17 -0600

    fix add fin del .env si existe

--- src/Console/CopyEnvExample.php
+++ src/Console/CopyEnvExample.php
@@ -16,18 +16,19 @@ class CopyEnvExample extends Command
         $envPath = base_path('.env');
         $examplePath = base_path('vendor/kaelytechnology/cms-core/.env.example');
 
-        if (!$filesystem->exists($envPath)) {
-            if ($filesystem->exists($examplePath)) {
-                $envContent = $filesystem->get($examplePath);
-                $envContent = "# === Variables de entorno para cms-core (inicio) ===\n"
-                    . $envContent . "\n# === Variables de entorno para cms-core (fin) ===\n";
-                $filesystem->put($envPath, $envContent);
+        if ($filesystem->exists($examplePath)) {
+            $envVars = $filesystem->get($examplePath);
+            $envVars = "# === Variables de entorno para cms-core (inicio) ===\n"
+                . $envVars . "\n# === Variables de entorno para cms-core (fin) ===\n";
+            if (!$filesystem->exists($envPath)) {
+                $filesystem->put($envPath, $envVars);
                 $this->info('.env copiado desde .env.example con comentarios de identificación');
             } else {
-                $this->error('No se encontró .env.example en el paquete.');
+                $filesystem->append($envPath, "\n" . $envVars);
+                $this->info('Variables de cms-core agregadas al final de .env existente.');
             }
         } else {
-            $this->info('El archivo .env ya existe.');
+            $this->error('No se encontró .env.example en el paquete.');
         }
     }
 }
